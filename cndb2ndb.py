bc2565cec2d8acf8d02d149d5226aa4d8d389916
Matheus Mello <mfmello@outlook.com>
2023-07-28 10:38:12 -0500

    fix: subcompartment sequence as `str` instead of `int`

--- cndb2ndb.py
+++ cndb2ndb.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
-#coding: utf8
+# coding: utf8
 
-__description__ = \
-"""
+__description__ = """
 Converting *.cndb to Nucleome Data Bank format .ndb
 """
 
 __author__ = "Vinícius Contessoto / Matheus Mello / Antonio B. Oliveira Junior"
-__date__   = "Set/2020"
+__date__ = "Set/2020"
 
 ################################################################
 #
@@ -23,121 +22,175 @@ import time
 import argparse
 import numpy as np
 import h5py
-from   itertools import islice
+from itertools import islice
 
-parser = argparse.ArgumentParser(description='Converting from *.cndb to *.ndb file')
-parser.add_argument('-f', metavar='input-file-CNDB-frames',help='cndb file', action='store', dest='arg_fname', required=True)
-parser.add_argument('-res', action='store', default=50000, dest='arg_res', help='Resolution for each simulation bead')
-parser.add_argument('-chroID', action='store', default='C', dest='arg_chroID', help='Chain ID')
-parser.add_argument('-n', action='store', default='chromatin', dest='arg_name',  help='Name of output file')
-parser.add_argument('-loops', metavar='input-file-Loops',help='Loops contact pair between i and j',type=argparse.FileType('rt'), required=False)
-parser.add_argument('-sigma', action='store', default=0.000, dest='arg_sigma', help='Distance fluctuation')
-parser.add_argument('-scale', action='store', default=1.000, dest='arg_scale', help='Distance scale')
+parser = argparse.ArgumentParser(description="Converting from *.cndb to *.ndb file")
+parser.add_argument(
+    "-f",
+    metavar="input-file-CNDB-frames",
+    help="cndb file",
+    action="store",
+    dest="arg_fname",
+    required=True,
+)
+parser.add_argument(
+    "-res",
+    action="store",
+    default=50000,
+    dest="arg_res",
+    help="Resolution for each simulation bead",
+)
+parser.add_argument(
+    "-chroID", action="store", default="C", dest="arg_chroID", help="Chain ID"
+)
+parser.add_argument(
+    "-n",
+    action="store",
+    default="chromatin",
+    dest="arg_name",
+    help="Name of output file",
+)
+parser.add_argument(
+    "-loops",
+    metavar="input-file-Loops",
+    help="Loops contact pair between i and j",
+    type=argparse.FileType("rt"),
+    required=False,
+)
+parser.add_argument(
+    "-sigma",
+    action="store",
+    default=0.000,
+    dest="arg_sigma",
+    help="Distance fluctuation",
+)
+parser.add_argument(
+    "-scale", action="store", default=1.000, dest="arg_scale", help="Distance scale"
+)
 
 try:
     arguments = parser.parse_args()
-    print('################################################')
-    print('Chosen file: {:}'.format(arguments.arg_fname))
-    print('Resolution: {:}'.format(arguments.arg_res))
-    print('Chrom ID: {:}'.format(arguments.arg_chroID))
-    print('Distance scale: {:}'.format(arguments.arg_scale))
+    print("################################################")
+    print("Chosen file: {:}".format(arguments.arg_fname))
+    print("Resolution: {:}".format(arguments.arg_res))
+    print("Chrom ID: {:}".format(arguments.arg_chroID))
+    print("Distance scale: {:}".format(arguments.arg_scale))
 
 except IOError as msg:
     parser.error(str(msg))
 
-Main_chrom      = ['ChrA','ChrB','ChrU'] # Type A B and Unknow
-Chrom_types     = ['ZA','OA','FB','SB','TB','LB','UN']
-Chrom_types_NDB = ['A1','A2','B1','B2','B3','B4','UN']
+Main_chrom = ["ChrA", "ChrB", "ChrU"]  # Type A B and Unknow
+Chrom_types = ["ZA", "OA", "FB", "SB", "TB", "LB", "UN"]
+Chrom_types_NDB = ["A1", "A2", "B1", "B2", "B3", "B4", "UN"]
 
 ##################################################################################################
 
 b_time = time.time()
 
-gro_string     = "{0:5d}{1:5s}{2:5s}{3:5d}{4:8.3f}{5:8.3f}{6:8.3f}"
+gro_string = "{0:5d}{1:5s}{2:5s}{3:5d}{4:8.3f}{5:8.3f}{6:8.3f}"
 gro_box_string = "{0:10.5f}{1:10.5f}{2:10.5f}"
-ndb_string     = "{0:6s} {1:8d} {2:2s} {3:6s} {4:4s} {5:8d} {6:8.3f} {7:8.3f} {8:8.3f} {9:10d} {10:10d} {11:8.3f}"
-pdb_string     = "{:6s}{:5d} {:^4s}{:1s}{:3s} {:1s}{:4d}{:1s}   {:8.3f}{:8.3f}{:8.3f}{:6.2f}{:6.2f}          {:>2s}{:2s}"
-header_string  = "{0:6s}    {1:40s}{2:9s}   {3:4s}\n"
-title_string   = "{0:6s}  {1:2s}{2:80s}\n"
-author_string  = "{0:6s}  {1:2s}{2:79s}\n"
+ndb_string = "{0:6s} {1:8d} {2:2s} {3:6s} {4:4s} {5:8d} {6:8.3f} {7:8.3f} {8:8.3f} {9:10d} {10:10d} {11:8.3f}"
+pdb_string = "{:6s}{:5d} {:^4s}{:1s}{:3s} {:1s}{:4d}{:1s}   {:8.3f}{:8.3f}{:8.3f}{:6.2f}{:6.2f}          {:>2s}{:2s}"
+header_string = "{0:6s}    {1:40s}{2:9s}   {3:4s}\n"
+title_string = "{0:6s}  {1:2s}{2:80s}\n"
+author_string = "{0:6s}  {1:2s}{2:79s}\n"
 expdata_string = "{0:6s}  {1:2s}{2:79s}\n"
-model_string   = "{0:6s}     {1:4d}"
-seqchr_string  = "{0:6s} {1:3d} {2:2s} {3:5d}  {4:69s}\n"
-ter_string     = "{0:6s} {1:8d} {2:2s}        {3:2s}"
-loops_string   = "{0:6s} {1:8d} {2:8d}"
-master_string  = "{0:6s} {1:8d} {2:6d} {3:6d} {4:10d}"   # MASTER BEADS TER LOOPS RES
-Type_conversion = {0:'A1',1:'A2',2:'B1',3:'B2',4:'B3',5:'B4',6:'UN'}
+model_string = "{0:6s}     {1:4d}"
+seqchr_string = "{0:6s} {1:3d} {2:2s} {3:5d}  {4:69s}\n"
+ter_string = "{0:6s} {1:8d} {2:2s}        {3:2s}"
+loops_string = "{0:6s} {1:8d} {2:8d}"
+master_string = "{0:6s} {1:8d} {2:6d} {3:6d} {4:10d}"  # MASTER BEADS TER LOOPS RES
 
-
-filename   = arguments.arg_fname
+filename = arguments.arg_fname
 file_loops = arguments.loops
-res        = np.int(arguments.arg_res)
-chroID     = np.str(arguments.arg_chroID)
-sigma      = np.float(arguments.arg_sigma)
-scale      = np.float(arguments.arg_scale)
+res = np.int(arguments.arg_res)
+chroID = np.str(arguments.arg_chroID)
+sigma = np.float(arguments.arg_sigma)
+scale = np.float(arguments.arg_scale)
 
-ndbf       = open(arguments.arg_name+'.ndb', "w+")
+ndbf = open(arguments.arg_name + ".ndb", "w+")
 
-mode = 'r'
+mode = "r"
 cndbf = h5py.File(filename, mode)
 
-type_list=list(cndbf['types'])
+type_list = list(cndbf["types"].asstr())
 
 ### [ HEADER ]
-ndbf.write(header_string.format('HEADER','NDB File generated by Open-MiChroM'," ", " "))
-ndbf.write(title_string.format('TITLE ','  ','A Scalable Computational Approach for '))
-ndbf.write(title_string.format('TITLE ','2 ','Simulating Complexes of Multiple Chromosomes'))
-ndbf.write(expdata_string.format('EXPDTA','  ','Cell Line  @50k bp resolution'))
-ndbf.write(expdata_string.format('EXPDTA','  ','Simulation - Open-MiChroM'))
-ndbf.write(author_string.format('AUTHOR','  ','Antonio B. Oliveira Junior - 2020'))
+ndbf.write(
+    header_string.format("HEADER", "NDB File generated by Open-MiChroM", " ", " ")
+)
+ndbf.write(
+    title_string.format("TITLE ", "  ", "A Scalable Computational Approach for ")
+)
+ndbf.write(
+    title_string.format("TITLE ", "2 ", "Simulating Complexes of Multiple Chromosomes")
+)
+ndbf.write(expdata_string.format("EXPDTA", "  ", "Cell Line  @50k bp resolution"))
+ndbf.write(expdata_string.format("EXPDTA", "  ", "Simulation - Open-MiChroM"))
+ndbf.write(author_string.format("AUTHOR", "  ", "Antonio B. Oliveira Junior - 2020"))
+
 
 # [ Creating SEQCHR ]
 def chunks(l, n):
     n = max(1, n)
-    return ([l[i:i+n] for i in range(0, len(l), n)])
+    return [l[i : i + n] for i in range(0, len(l), n)]
 
-Seqlist = [Type_conversion[x] for x in type_list]
-Seqlista = chunks(Seqlist,23)
 
-for num, line in enumerate(Seqlista):
-    ndbf.write(seqchr_string.format("SEQCHR", num+1, "C1", 
-                                     len(type_list)," ".join(line)))
+seq_chunks= chunks(type_list, 23)
+
+for num, line in enumerate(seq_chunks):
+    ndbf.write(
+        seqchr_string.format("SEQCHR", num + 1, "C1", len(type_list), " ".join(line))
+    )
 
 # [ Writing .ndb body ]
 model = 1
 chain = 1
 index_c = 0
-try: 
-    loop = np.array(cndbf['loops'])
-    totalModel = len(cndbf)-1
-    Wloop = True
+try:
+    loop = np.array(cndbf["loops"])
+    totalModel = len(cndbf) - 1
+    write_loops = True
 except KeyError:
     totalModel = len(cndbf)
-    Wloop = False
+    write_loops = False
 
-for mdl in range(1,totalModel,1):
-    ndbf.write(model_string.format('MODEL ', int(mdl)))
+for mdl in range(1, totalModel, 1):
+    ndbf.write(model_string.format("MODEL ", int(mdl)))
     ndbf.write("\n")
-    
-    data = np.array(cndbf[str(mdl)])   
-    
-    for i, line in zip(list(range(len(data))), data):    
-        ndbf.write(ndb_string.format('CHROM ', i+1, Seqlist[i], " ", "C1", i+1,
-                                    line[0], line[1], line[2], np.int((i) * 50000)+1, np.int(i * 50000+50000), 0)) # Aqui a gente escreve as coordenadas e os campos coloridos
+
+    data = np.array(cndbf[str(mdl)])
+
+    for i, line in zip(list(range(len(data))), data):
+        ndbf.write(
+            ndb_string.format(
+                "CHROM ",
+                i + 1,
+                type_list[i],
+                " ",
+                "C1",
+                i + 1,
+                line[0],
+                line[1],
+                line[2],
+                np.int((i) * 50000) + 1,
+                np.int(i * 50000 + 50000),
+                0,
+            )
+        )  
         ndbf.write("\n")
 
-
-    ndbf.write(ter_string.format('TER', len(data)+1, Seqlist[-1], 'C1'))
-    ndbf.write('\nENDMDL\n')
+    ndbf.write(ter_string.format("TER", len(data) + 1, type_list[-1], "C1"))
+    ndbf.write("\nENDMDL\n")
     model += 1
 print("Last traj frame:", model)
 
-if Wloop:
-    loop = np.sort(loop,axis=0)
+if write_loops:
+    loop = np.sort(loop, axis=0)
     for p in loop:
-        ndbf.write(loops_string.format("LOOPS",p[0],p[1]))
+        ndbf.write(loops_string.format("LOOPS", p[0], p[1]))
         ndbf.write("\n")
-ndbf.write('END')
+ndbf.write("END")
 ndbf.write("\n")
 
 ndbf.close()
--- ndb2cndb.py
+++ ndb2cndb.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
-#coding: utf8 
+# coding: utf8
 
-__description__ = \
-"""
+__description__ = """
 Converting *.ndb to *.cndb format
 """
 
 __author__ = "Vinícius Contessoto / Matheus Mello / Antonio B. Oliveira Junior"
-__date__   = "Sep/2020"
+__date__ = "Sep/2020"
 
 ################################################################
-# 
+#
 # Trajectories file *.ndb to Compacted Nucleome Data Bank format .cndb
 #
 # usage:
@@ -23,23 +22,34 @@ import argparse
 import numpy as np
 import h5py
 
-parser = argparse.ArgumentParser(description='Converting *.ndb to *.cndb format')
-parser.add_argument('-f', metavar='input-file-ndb-frames',help='ndb file with frames',type=argparse.FileType('rt'))
-parser.add_argument('-n', action='store', default='chromatin', dest='arg_name',  help='Name of output file')
+parser = argparse.ArgumentParser(description="Converting *.ndb to *.cndb format")
+parser.add_argument(
+    "-f",
+    metavar="input-file-ndb-frames",
+    help="ndb file with frames",
+    type=argparse.FileType("rt"),
+)
+parser.add_argument(
+    "-n",
+    action="store",
+    default="chromatin",
+    dest="arg_name",
+    help="Name of output file",
+)
 
 try:
     arguments = parser.parse_args()
-    print('################################################')
-    print('Chosen file: {:}'.format(arguments.f.name))
+    print("################################################")
+    print("Chosen file: {:}".format(arguments.f.name))
 
 except IOError as msg:
-    parser.error(str(msg))                    
+    parser.error(str(msg))
 
-Main_chrom      = ['ChrA','ChrB','ChrU'] # Type A B and Unknow
-Chrom_types     = ['ZA','OA','FB','SB','TB','LB','UN']
-Chrom_types_NDB = ['A1','A2','B1','B2','B3','B4','UN']
-Res_types_PDB   = ['ASP', 'GLU', 'ARG', 'LYS', 'HIS', 'HIS', 'GLY']
-Type_conversion = {'A1': 0,'A2' : 1,'B1' : 2,'B2' : 3,'B3' : 4,'B4' : 5,'UN' : 6}
+Main_chrom = ["ChrA", "ChrB", "ChrU"]  # Type A B and Unknow
+Chrom_types = ["ZA", "OA", "FB", "SB", "TB", "LB", "UN"]
+Chrom_types_NDB = ["A1", "A2", "B1", "B2", "B3", "B4", "UN"]
+Res_types_PDB = ["ASP", "GLU", "ARG", "LYS", "HIS", "HIS", "GLY"]
+Type_conversion = {"A1": 0, "A2": 1, "B1": 2, "B2": 3, "B3": 4, "B4": 5, "UN": 6}
 
 ##################################################################################################
 
@@ -47,31 +57,47 @@ b_time = time.time()
 
 # pdb formats              1         2         3
 #                 123456789012345678901234567890
-model          = "MODEL     {0:4d}"
-atom           = "ATOM  {0:5d} {1:^4s}{2:1s}{3:3s} {4:1s}{5:4d}{6:1s}   {7:8.3f}{8:8.3f}{9:8.3f}{10:6.2f}{11:6.2f}          {12:>2s}{13:2s}"
-ter            = "TER   {0:5d}      {1:3s} {2:1s}{3:4d}{4:1s}"
+model = "MODEL     {0:4d}"
+atom = "ATOM  {0:5d} {1:^4s}{2:1s}{3:3s} {4:1s}{5:4d}{6:1s}   {7:8.3f}{8:8.3f}{9:8.3f}{10:6.2f}{11:6.2f}          {12:>2s}{13:2s}"
+ter = "TER   {0:5d}      {1:3s} {2:1s}{3:4d}{4:1s}"
 
 file_ndb = arguments.f
-name     = arguments.arg_name
+name = arguments.arg_name
 
-cndbf = h5py.File(name + '.cndb', 'w')
+cndbf = h5py.File(name + ".cndb", "w")
 
-print('Converting file...')
+print("Converting file...")
 
-title_options = ['HEADER','OBSLTE','TITLE ','SPLT  ','CAVEAT','COMPND','SOURCE','KEYWDS','EXPDTA','NUMMDL','MDLTYP','AUTHOR','REVDAT','SPRSDE','JRNL  ','REMARK']
+title_options = [
+    "HEADER",
+    "OBSLTE",
+    "TITLE ",
+    "SPLT  ",
+    "CAVEAT",
+    "COMPND",
+    "SOURCE",
+    "KEYWDS",
+    "EXPDTA",
+    "NUMMDL",
+    "MDLTYP",
+    "AUTHOR",
+    "REVDAT",
+    "SPRSDE",
+    "JRNL  ",
+    "REMARK",
+]
 
 loop = 0
 types = []
 types_bool = True
 loop_list = []
 x = []
-y = [] 
+y = []
 z = []
 
 frame = 0
 
 for line in file_ndb:
-    
     entry = line[0:6]
 
     info = line.split()
@@ -80,28 +106,26 @@ for line in file_ndb:
 
     # [ Coodinate Section ]
 
-    if 'MODEL' in entry:
+    if "MODEL" in entry:
         frame += 1
 
         inModel = True
-    
-    elif 'CHROM' in entry:
 
+    elif "CHROM" in entry:
         subtype = line[16:18]
-        #print(subtype, line)
+        # print(subtype, line)
 
         types.append(subtype)
         x.append(float(line[40:48]))
         y.append(float(line[49:57]))
         z.append(float(line[58:66]))
 
-    elif 'ENDMDL' in entry:
+    elif "ENDMDL" in entry:
         if types_bool:
-            typelist = [Type_conversion[x] for x in types]
-            cndbf['types'] = typelist
+            cndbf["types"] = types
             types_bool = False
-        
-        positions = np.vstack([x,y,z]).T
+
+        positions = np.vstack([x, y, z]).T
         cndbf[str(frame)] = positions
         x = []
         y = []
@@ -109,17 +133,17 @@ for line in file_ndb:
 
     # [ Loops file ]
 
-    elif 'LOOPS' in entry:
+    elif "LOOPS" in entry:
         loop_list.append([int(info[1]), int(info[2])])
         loop += 1
-        
+
 if loop > 0:
-    cndbf['loops'] = loop_list
+    cndbf["loops"] = loop_list
 
 cndbf.close()
-print('Finished!')
+print("Finished!")
 
 
 e_time = time.time()
 elapsed = e_time - b_time
-print('Ran in %.3f sec' % elapsed)
+print("Ran in %.3f sec" % elapsed)
